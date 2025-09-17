fc29547ba1ba0759e09ab85c5b159489e9fdcb4c
smilyg1908@gmail.com <smilyg1908@gmail.com>
2025-09-17 10:50:14 +0300

    updations vite config file adding base

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: './',              // ensure relative paths, e.g. in index.html
+  build: {
+    outDir: 'dist',      // gets deployed to github pages
+    assetsDir: 'assets', // static assets (images, js, css, etc.)
+  },
 })
